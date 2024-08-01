70291e429dcbd727b496a846d4eb43c3e6b92315
Your Name <you@example.com>
2024-08-01 19:31:23 +0300

    complete image upload functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,30 @@
-import {FaSearch} from 'react-icons/fa'
+import { AiOutlineSearch } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import icon_g from '../assets/icon-g.png'
 import { useSelector } from 'react-redux';
+import { TextInput } from 'flowbite-react';
 export default function Header() {
     const { currentUser } = useSelector(state => state.user)
   return (
     <header className='bg-slate-200 shadow-md'>
-        {/* <div className='flex flex-row gap-3'>
-         <img src={icon_g} className='w-[190px] h-[50px]' alt="" srcset="" /> */}
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <Link to='/'>
+        <div className='flex flex-nowrap'>
+         <img src={icon_g} className='w-[190px] h-[50px] mt-3' alt="" />
+        <div className='flex gap-0 sm:gap-52 justify-between items-center max-w-6xl mx-auto p-3'>
+            {/* <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                 <span className='text-slate-500'>Mekelle</span>
                 <span className='text-slate-700'>Estate</span>
             </h1>
-            </Link>
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
-                <FaSearch className='text-slate-500' />
+            </Link> */}
+            <form>
+            <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        //   value={searchTerm}
+        //   onChange={(e) => setSearchTerm(e.target.value)}
+        />
             </form>
             <ul className='flex gap-4'>
                 <Link to='/'>
@@ -33,7 +40,7 @@ export default function Header() {
                 </Link>            
             </ul>
         </div>
-        {/* </div> */}
+        </div>
     </header>
   )
 }
--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,7 +1,132 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import { Button, TextInput, Alert } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { CircularProgressbar } from "react-circular-progressbar";
+import { app } from "../firebase"
+import "react-circular-progressbar/dist/styles.css";
 
-export default function profile() {
+export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [filePerc, setFilePerc] = useState(0);
+  const [file, setFile] = useState(undefined);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const filePickerRef = useRef();
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if(file) {
+      handleFileUpload(file);
+    }
+  }, [file])
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress))
+      },
+    (error)=>{
+      setFileUploadError(
+        'Could not upload image (File must be less than 2MB)'
+      );
+    },
+    ()=> {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setFormData({ ...formData, avatar: downloadURL });
+      });
+    })
+  }
+
   return (
-    <div>profile</div>
-  )
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input 
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
+          {filePerc > 0 && filePerc < 100 && (
+            <CircularProgressbar
+              value={filePerc || 0}
+              text={`${filePerc}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    filePerc / 100
+                  })`,
+                },
+              }}
+            />
+          )}
+          <img
+            src={ formData.avatar || currentUser.avatar}
+            alt="user"
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              filePerc &&
+              filePerc < 100 &&
+              "opacity-60"
+            }`}
+          />
+        </div>
+        {fileUploadError && (
+          <Alert color='failure'>{fileUploadError}</Alert>
+        )}
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+          // onChange={handleChange}
+        />
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+          // onChange={handleChange}
+        />
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="password"
+          // onChange={handleChange}
+        />
+        <Button
+          type="submit"
+          gradientDuoTone="purpleToBlue"
+          outline
+          // disabled={loading || imageFileUploading}
+        >
+          Update
+        </Button>
+      </form>
+      <div className="text-red-700 flex justify-between mt-5">
+        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">
+          <Link to="/">Sign Out</Link>
+        </span>
+      </div>
+    </div>
+  );
 }
