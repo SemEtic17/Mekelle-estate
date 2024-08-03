df37414f949c23c7248d0f90f90b3d1ff735eeb3
Your Name <you@example.com>
2024-08-03 16:56:08 +0300

    add create listing api route

---
+++ api/routes/listing.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { createLinsing } from '../controllers/listing.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+const router = express.Router();
+
+router.post('/create', verifyToken, createLinsing);
+
+export default router;
