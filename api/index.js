0eec852449cafe5567b0a2834488784f632cb823
SemEtic17 <semetic17@gmail.com>
2024-08-13 12:01:48 -0700

    add preinstallation

--- api/index.js
+++ api/index.js
@@ -1,23 +1,29 @@
-import express from 'express';
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-import listingRouter from './routes/listing.route.js'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.MONGODB_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.MONGODB_PASSWORD
+);
 
 mongoose
   .connect(DB)
   .then(() => {
-    console.log('MongoDb is connected');
+    console.log("MongoDb is connected");
   })
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -26,12 +32,18 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-    console.log('server running on port 3000')
-})
+  console.log("server running on port 3000");
+});
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/listing', listingRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
