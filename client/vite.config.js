50bd2be31e3db695fb1cda13223af4cda976cbcb
Sem Dori <semetic17@gmail.com>
2024-09-18 05:28:56 -0700

    Adding PWA functionality using vite-plugin-pwa and icons

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "http://localhost:3000",
         secure: false,
-      }
-    }
+      },
+    },
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "mke-logo.png",
+        "mke-logo.svg",
+        "house/scene.gltf",
+      ],
+      manifest: {
+        name: "Mekelle Estate",
+        short_name: "Mekelle Estate",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
+});
